bbaa9a1b5df6d8b7ea9fedbaeab3c93da35126d7
Daniil Shestopalov <d.shestopalov@letmecode.org>
2024-12-02 09:46:23 +0300

    create project

---
+++ Alamofire/Alamofire/AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireApp.swift
+//  Alamofire
+//
+//  Created by Daniil on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alamofire/Alamofire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alamofire
+//
+//  Created by Daniil on 02.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
